2ee97d65d8880209162060d6f7559c8e4c0a44e2
Jimmy McNamara <jmcnamara44@gmail.com>
2018-07-20 16:54:18 -0700

    test the google places api to try and understand the format it will come back in.  add placeholder and basic plan into pointsOfInterest component.

--- src/components/PointsOfInterest.jsx
+++ src/components/PointsOfInterest.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function PointsOfInterest(props){
+
+
   return (
     <div>
-      <p>Placeholder for POIs</p>
+      <p>Attractions in your area:</p>
+      <ul>
+        // <li>Here will be a list of attractions from the google places api, most likely using latitude and longitude.  This is a placeholder, I am working and testing the results from the api</li>
+      </ul>
     </div>
   );
 }
